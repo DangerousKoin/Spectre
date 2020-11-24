efc1d80957d0802934764defbd1759f48655a471
kbreiten <kory.breiten@gmail.com>
2020-11-24 11:17:51 -0800

    Cleanup

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/users');
 
-
 router.get('/management/users', userCtrl.index);
-
-// router.get('/users/new', userCtrl.new);
-// router.post('/users', userCtrl.create);
-// router.post('/users/:id/', userCtrl.addToUserList);
-
-// router.get('/', function(req, res, next) {
-//     res.render('/management/users');
-//   });
 
 module.exports = router;
