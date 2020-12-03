e93e7411a6c89d39ae72a4b3f6367ef50c271802
kbreiten <kory.breiten@gmail.com>
2020-12-03 04:56:27 -0800

    More Route fixing

--- controllers/userCont.js
+++ controllers/userCont.js
@@ -29,30 +29,27 @@ function cart(req, res) {
   });
 }
 
-function addToCart(err, req, res, next) {
-  User.findById(req.params.id, function (err, user, next) {
+function addToCart(req, res) {
+  User.findById(req.body.userId, function (err, user, next) {
     if (err) return next(err);
     user.cart.push(req.body.itemId);
     user.save();
-    res.redirect('/user/cart');
+    res.redirect('/cart');
   });
-  if (err) return next(err);
 }
 
-function delUser(err, req, res, next) {
-  User.findById(req.params.id).exec(function(err, user, next) {
+function delUser(req, res) {
+  User.findById(req.body.userId).exec(function(err, user, next) {
     if (err) return next(err);
     user.remove();
     res.redirect('/peace');
     });
-  if (err) return next(err);
 }
 
-function delFromCart(err, req, res, next) {
+function delFromCart(req, res) {
   User.findById(req.body.userId, function (err, user, next) {
     if (err) return next(err);
     user.cart.remove(req.body.itemId);
-    res.redirect('/user/cart');
+    res.redirect('/cart');
   });
-  if (err) return next(err);
 }
